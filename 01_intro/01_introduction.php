d9cb9db7de1545d836906149244c796c7da98ebf
Sleyter Monteiro <sleytermonteiro92@protonmail.com>
2021-12-10 14:35:29 +0100

    initiation PHP

---
+++ 01_intro/01_introduction.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Cours PHP - Introduction</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <header class="container-fluid text-center">
+        <h1 class="display-4">CoursPHP - Introduction</h1>
+        <p class="lead">PHP : Php Hypertext Preprocessor</p>
+    </header>
+
+    <div class="container bg-white p-2">
+        <section class="row">
+            <div class="col-md-4">
+                <h4>1/ Réaliser des sites dynamique</h4>
+                <p>Pour réaliser un site dynamique nous allons aborder les points suivants : </p>
+                <ul>
+                    <li>La syntaxe et les caractéristiques du language PHP (v7)</li>
+                    <li>Les notions essentielles du language SQL permettant la gestion d'une BDD et la réalisation ddes requêtes de base</li>
+                    <li>... et les moyens d'y accéder à l'aide de fonctions spécialisées de PHP (ou d'objet)</li>
+                </ul>
+            </div>
+            <!-- fin col -->
+            <div class="col-md-4">
+                <h4>2/ PHP c'est quoi ?</h4>
+            </div>
+            <!-- fin col -->
+            <div class="col-md-4">
+                <h4></h4>
+            </div>
+            <!-- fin col -->
+        </section>
+        <!-- fin row -->
+    </div>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # coursPHP
 Initiation PHP Colombbus
+
+
+DOSSIERS
+## 00-exos
+## 00-pages
+    ### 01_ma_page_php.php
+## 01-intro
+    ### 01_introduction.php
+    ### infos.php
+## 02-variables
+    ### 01_variables.php
+    ### 02_types.php
+    ### 03_chaines.php
+    ### 04_tableaux.php
+## 03-conditions
+    ### 01_conditions.php
+    ### 02_boucles.php
+## 04-get
+    ### 01_method_get.php
+    ### 02_method_get.php
+## 05-post
+    ### 01_method_post.php
+## 06-pdo
+    ### 01_pdo.php
+## 07-cookies
+    ### 01_cookies.php
+## 08-session
+    ### 01_session.php
+    ### 02_session.php
+    ### 03_session.php
+## 09-securite
+    ### 01_dialogue.php
+    ### 02_employes.php
+    ### 03_employes_suite.php
+    ### 04_fiche_employe.php
+
+## inc
+    ### functions.php
+    ### head.inc.php
+    ### body.inc.php
+    ### nav.inc.php
+    ### footer.inc.php
+
+## 10-boutique
+    SOMMAIRE A COMPLETER
+
+
+## css
+## img
+## js
